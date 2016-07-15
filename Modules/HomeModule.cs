c0fc058dc629e93a4be00931064aa7dd243df544
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-07-15 15:49:53 -0700

    add forms for creating and clients and stylists

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -31,10 +31,40 @@ namespace HairSalon
         Dictionary<string, object> Model = new Dictionary<string, object>();
         var foundStylist = Stylist.Find(parameters.id);
         var foundClients = foundStylist.GetClients();
+        var foundApppointments = foundStylist.GetAppointments();
         Model.Add("stylist", foundStylist);
         Model.Add("clients", foundClients);
+        Model.Add("appointments", foundApppointments);
         return View["stylist.cshtml", Model];
       };
+
+
+
+      Get["/client/new"] = _ => {
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View ["client_form.cshtml", allStylists];
+      };
+      Post["/client/new"] = _ => {
+        Client newClient = new Client(Request.Form["client_name"], Request.Form["stylist_id"]);
+        newClient.Save();
+        return View["success.cshtml", newClient];
+      };
+      Get["/client/all"] = _ => {
+        List<Client> allClients = Client.GetAll();
+        return View["clients.cshtml", allClients];
+      };
+      Get["client/{id}"] = parameters => {
+        Dictionary<string, object> Model = new Dictionary<string, object>();
+        var foundClient = Client.Find(parameters.id);
+        var foundStylist = Stylist.Find(foundClient.GetStylistId());
+
+        var foundAppointments = foundClient.GetAppointments();
+        Model.Add("stylist", foundStylist);
+        Model.Add("client", foundClient);
+        Model.Add("appointments", foundAppointments);
+        return View["client.cshtml", Model];
+      };
+
     }
   }
 }
