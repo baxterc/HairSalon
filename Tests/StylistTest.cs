f5490d85a1f260affea5219639f7d5fb22f89325
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-07-15 07:59:09 -0700

    add getters and setters for cuisine, add Database.cs to allow database connection, add GetAll method for cuisine and passing test for getting the database while empty

---
+++ Tests/StylistTest.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  public class StylistTest
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_GetAll_StylistTableEmpty()
+    {
+      int result = Stylist.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+  }
+}
